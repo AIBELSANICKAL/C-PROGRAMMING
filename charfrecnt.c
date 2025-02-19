f4ed265c7e3327a08bbd7acc48951c1db18f9f03
AIBELSANICKAL <aibelsanickal2028@cs.sjcetpalai.ac.in>
2025-02-19 16:31:14 +0530

    Add files via upload

---
+++ charfrecnt.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+	char istring[200];
+	int freq[26];
+	printf("\nenter the string:");
+	fgets(istring,sizeof(istring),stdin);
+	for(int i=0;istring[i]!='\0';i++){
+	   char ch=istring[i];
+	   if(istring[i]>='A' && istring[i]<='Z'){
+	        ch=ch+32;
+	    }
+           if(ch>='a'&& ch<='z'){
+                freq[ch-97]++;
+        }
+   }
+   for(int i=0;i<26;i++){
+     if (freq[i]!=0){
+   printf("%c:%d",i+97,freq[i]);
+   }
+   }
+   return 0;
+}             
+	   
+    
+
