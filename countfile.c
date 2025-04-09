acb1c4faa24f831295b4472b767567226ea7bcf0
AIBEL S ANICKAL <aibelsanickal2028@cs.sjcetpalai.ac.in>
2025-04-09 16:09:00 +0530

    Add files via upload

---
+++ countfile.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+   FILE *fp;
+   int wordcount=0;
+   fp= fopen("sample.txt","r");
+   char ch;
+   while(fscanf(fp,"%c",&ch)==1){
+       if(ch==' '|| ch=='.'){
+              wordcount++;
+       }       
+   
+    }
+    printf("word count=%d",wordcount);
+    fclose(fp);
+    return 0;
+}    
